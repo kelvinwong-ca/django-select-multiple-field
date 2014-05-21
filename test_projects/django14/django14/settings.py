a7eeb9df228ee1311a4b3a3763526d1b00ead6de
Kelvin Wong <code@kelvinwong.ca>
2014-05-21 16:11:30 -0700

    Demo has two languages

--- test_projects/django14/django14/settings.py
+++ test_projects/django14/django14/settings.py
@@ -36,7 +36,7 @@ TIME_ZONE = 'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
 SITE_ID = 1
 
@@ -47,6 +47,12 @@ USE_I18N = True
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale.
 USE_L10N = True
+
+ugettext = lambda s: s
+LANGUAGES = (
+    ('en', ugettext('English')),
+    ('fr', ugettext('French')),
+)
 
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
