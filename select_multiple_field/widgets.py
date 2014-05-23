f26202f688f7612971e35b0ae33a2f961a117876
Kelvin Wong <code@kelvinwong.ca>
2014-05-23 02:57:02 -0700

    Use format_html if it is available, fallback for dj 1.4

--- select_multiple_field/widgets.py
+++ select_multiple_field/widgets.py
@@ -5,6 +5,12 @@ from django.forms import widgets
 from django.forms.util import flatatt
 from django.utils.safestring import mark_safe
 
+try:
+    from django.utils.html import format_html
+except ImportError:
+    def format_html(format_string, *args, **kwargs):
+        return format_string.format(*args, **kwargs)
+
 
 class SelectMultipleField(widgets.SelectMultiple):
     """Multiple select widget ready for jQuery multiselect.js"""
@@ -19,7 +25,8 @@ class SelectMultipleField(widgets.SelectMultiple):
 
         final_attrs = self.build_attrs(rendered_attrs, name=name)
         # output = [u'<select multiple="multiple"%s>' % flatatt(final_attrs)]
-        output = ['<select multiple="multiple"{0}>'.format(flatatt(final_attrs))]
+        output = [format_html('<select multiple="multiple"{0}>',
+                              flatatt(final_attrs))]
         options = self.render_options(choices, value)
         if options:
             output.append(options)
