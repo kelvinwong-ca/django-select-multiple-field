954b7138d2f7bf7e554a79a26f9e988321321798
Kelvin Wong <code@kelvinwong.ca>
2014-05-22 21:05:00 -0700

    decruft

--- test_suite/test_codecs.py
+++ test_suite/test_codecs.py
@@ -2,7 +2,6 @@
 from __future__ import unicode_literals
 
 from django.test import SimpleTestCase
-from django.utils.six.moves import xrange
 
 from select_multiple_field.codecs import (
     DEFAULT_DELIMITER, decode_csv_to_list, encode_list_to_csv)
