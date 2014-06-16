5dcd01576e6f34e0257606d07ac198bf5ad68197
Kelvin Wong <code@kelvinwong.ca>
2014-06-16 04:40:43 -0700

    New demo app tests and models

--- test_projects/django14/forthewing/tests.py
+++ test_projects/django14/forthewing/tests.py
@@ -1,16 +1,86 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-Replace this with more appropriate tests for your application.
-"""
+from django.core.urlresolvers import reverse
+from django.test import SimpleTestCase, TestCase
+from django.utils.datastructures import MultiValueDict
+from django.utils.http import urlencode
 
-from django.test import TestCase
+from .models import ChickenWings, show_flavour
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class ChickenWingsListViewTestCase(TestCase):
+
+    def test_no_pizzas(self):
+        p = ChickenWings.objects.all()
+        self.assertEqual(len(p), 0, 'Test requires no wings')
+        response = self.client.get(reverse('ftw:list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('No chicken wings found' in response.content.decode('utf-8'))
+
+    def test_many_pizzas(self):
+        NUM_WINGS = 30
+        wings = []
+        for n in range(NUM_WINGS):
+            p = ChickenWings.objects.create(flavour=[ChickenWings.HONEY_BBQ])
+            wings.append(p)
+
+        self.assertEqual(len(wings), NUM_WINGS, 'Test requires chicken wings')
+        response = self.client.get(reverse('ftw:list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(
+            (ChickenWings.HONEY_BBQ) in response.content.decode('utf-8'))
+
+
+class ChickenWingsCreateViewTestCase(TestCase):
+
+    def test_view(self):
+        response = self.client.get(reverse('ftw:create'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_creation_single(self):
+        data = {
+            'flavour': [ChickenWings.JERK]
+        }
+        response = self.client.post(
+            reverse('ftw:create'),
+            urlencode(MultiValueDict(data), doseq=True),
+            content_type='application/x-www-form-urlencoded'
+            )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response,
+            'http://testserver' + reverse('pizza:created'))
+        p = ChickenWings.objects.all()[0]
+        self.assertIn(ChickenWings.JERK, p.flavour)
+
+    def test_creation_multiple(self):
+        data = {
+            'toppings': [ChickenWings.SUICIDE, ChickenWings.BOURBON]
+        }
+        response = self.client.post(
+            reverse('ftw:create'),
+            urlencode(MultiValueDict(data), doseq=True),
+            content_type='application/x-www-form-urlencoded'
+            )
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response,
+            'http://testserver' + reverse('ftw:created'))
+        p = Pizza.objects.all()[0]
+        self.assertIn(ChickenWings.SUICIDE, p.flavour)
+        self.assertIn(ChickenWings.BOURBON, p.flavour)
+
+
+class ChickenWingsModelTestCase(SimpleTestCase):
+
+    def test_show_flavour(self):
+        for k, v in Pizza.TOPPING_CHOICES:
+            if isinstance(v, (list, tuple)):
+                for ko, vo in v[1]:
+                    flavour_name = show_flavour(ko)
+                    self.assertEqual(flavour_name, vo)
+            else:
+                flavour_name = show_flavour(k)
+                self.assertEqual(flavour_name, v)
--- test_projects/django14/forthewing/views.py
+++ test_projects/django14/forthewing/views.py
@@ -1,1 +1,24 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import (
+    CreateView, DetailView, DeleteView, ListView, UpdateView)
+from django.utils.encoding import force_text
+
+from .models import ChickenWings
+
+
+class ChickenWingsListView(ListView):
+
+    queryset = ChickenWings.objects.order_by('-id')
+    context_object_name = 'chickenwings'
+    paginate_by = 10
+
+
+class ChickenWingsCreateView(CreateView):
+
+    model = ChickenWings
+    fields = ['flavour']
+    success_url = reverse_lazy('ftw:created')
+    context_object_name = 'wings'
