fc178200b44bbdb455d6606469e284ed8208cd0f
Kelvin Wong <code@kelvinwong.ca>
2014-06-17 00:04:09 -0700

    Rest of views

--- test_projects/django14/forthewing/views.py
+++ test_projects/django14/forthewing/views.py
@@ -22,3 +22,24 @@ class ChickenWingsCreateView(CreateView):
     fields = ['flavour']
     success_url = reverse_lazy('ftw:created')
     context_object_name = 'wings'
+
+
+class ChickenWingsDetailView(DetailView):
+
+    model = ChickenWings
+
+
+class ChickenWingsUpdateView(UpdateView):
+
+    model = ChickenWings
+    fields = ['flavour']
+    success_url = reverse_lazy('ftw:updated')
+
+
+class ChickenWingsDeleteView(DeleteView):
+
+    model = ChickenWings
+    success_url = reverse_lazy('ftw:deleted')
+
+    def get_success_url(self):
+        return force_text(self.success_url)
