f3f617934dc29f3236bb3691a3c8d5c7c738d0f2
Kelvin Wong <code@kelvinwong.ca>
2015-02-09 23:51:50 -0800

    Silenced pep8 warnings

--- select_multiple_field/validators.py
+++ select_multiple_field/validators.py
@@ -8,30 +8,39 @@ from django.utils.translation import ungettext_lazy
 try:
     from django.utils.deconstruct import deconstructible
 except ImportError:
-    deconstructible = lambda x: x
+    def deconstructible(x):
+        return x
 
 from .codecs import encode_list_to_csv
 
 
 @deconstructible
 class MaxChoicesValidator(validators.BaseValidator):
-    compare = lambda self, a, b: a > b
-    clean = lambda self, x: len(x)
+
     message = ungettext_lazy(
         'Ensure this value has at most %(limit_value)d choice (it has %(show_value)d).',  # NOQA
         'Ensure this value has at most %(limit_value)d choices (it has %(show_value)d).',  # NOQA
         'limit_value')
     code = 'max_choices'
 
+    def compare(self, a, b):
+        return a > b
+
+    def clean(self, x):
+        return len(x)
+
 
 @deconstructible
 class MaxLengthValidator(validators.BaseValidator):
-    compare = lambda self, a, b: a > b
+
     message = ungettext_lazy(
         'Ensure this value has at most %(limit_value)d character (it has %(show_value)d).',  # NOQA
         'Ensure this value has at most %(limit_value)d characters (it has %(show_value)d).',  # NOQA
         'limit_value')
     code = 'max_length'
+
+    def compare(self, a, b):
+        return a > b
 
     def clean(self, value):
         return len(force_text(encode_list_to_csv(value)))
