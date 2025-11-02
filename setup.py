b4958d960bcf038f0344660f7cdddd08930573fe
Kelvin Wong <code@kelvinwong.ca>
2025-11-02 00:59:08 -0700

    Attempt to use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
-from distutils.core import setup, Command
+from setuptools import setup, Command
 import os
 import re
 import sys
 
-from select_multiple_field import __version__
+REPO_BASE = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version():
+    """Read version without importing the package"""
+
+    version_file = os.path.join(REPO_BASE, "select_multiple_field", "__init__.py")
+    with open(version_file) as f:
+        for line in f:
+            if line.startswith("__version__"):
+                return line.split("=")[1].strip().strip("'\"")
+    raise RuntimeError("Unable to find version string.")
+
+
+__version__ = get_version()
 
 
 cmdclasses = dict()
-README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                           'README.rst')
-long_description = open(README_PATH, 'r').read()
+README_PATH = os.path.join(REPO_BASE, "README.rst")
+long_description = open(README_PATH, "r").read()
 
 
 class DemoTester(Command):
@@ -18,13 +31,13 @@ class DemoTester(Command):
 
     user_options = []
     test_settings = {
-        '1.4': 'test_projects.django14.django14.settings',
-        '1.5': 'test_projects.django14.django14.settings',
-        '1.6': 'test_projects.django14.django14.settings',
-        '1.7': 'test_projects.django14.django14.settings',
-        '1.8': 'test_projects.django18.django18.settings',
-        '1.9': 'test_projects.django18.django18.settings',
-        '1.10': 'test_projects.django18.django18.settings',
+        "1.4": "test_projects.django14.django14.settings",
+        "1.5": "test_projects.django14.django14.settings",
+        "1.6": "test_projects.django14.django14.settings",
+        "1.7": "test_projects.django14.django14.settings",
+        "1.8": "test_projects.django18.django18.settings",
+        "1.9": "test_projects.django18.django18.settings",
+        "1.10": "test_projects.django18.django18.settings",
     }
 
     def initialize_options(self):
@@ -35,11 +48,12 @@ class DemoTester(Command):
 
     def run(self):
         sys.dont_write_bytecode = True
-        from django import get_version
-        django_release = re.search(r'^\d\.\d+', get_version()).group(0)
+        from django import get_version as django_get_version
+
+        django_release = re.search(r"^\d\.\d+", django_get_version()).group(0)
         test_settings_exist = django_release in self.test_settings.keys()
         try:
-            dj_ver = [int(n) for n in re.split(r'[.ab]', get_version())]
+            dj_ver = [int(n) for n in re.split(r"[.ab]", django_get_version())]
         except ValueError:
             # Pre-release Djangos must be testable!!!
             dj_too_old = False
@@ -49,8 +63,7 @@ class DemoTester(Command):
         if test_settings_exist is False or dj_too_old:
             print("Please install Django 1.4.19 - 1.10 to run the test suite")
             exit(-1)
-        os.environ['DJANGO_SETTINGS_MODULE'] = self.test_settings[
-            django_release]
+        os.environ["DJANGO_SETTINGS_MODULE"] = self.test_settings[django_release]
         try:
             from django.core.management import call_command
         except ImportError:
@@ -58,21 +71,22 @@ class DemoTester(Command):
             exit(-1)
 
         import django
-        if hasattr(django, 'setup'):
+
+        if hasattr(django, "setup"):
             django.setup()
 
-        call_command('test', 'pizzagigi', interactive=False, verbosity=1)
-        call_command('test', 'forthewing', interactive=False, verbosity=1)
+        call_command("test", "pizzagigi", interactive=False, verbosity=1)
+        call_command("test", "forthewing", interactive=False, verbosity=1)
 
         try:
             import south
         except ImportError:
             pass
         else:
-            call_command('test', 'suthern', interactive=False, verbosity=1)
+            call_command("test", "suthern", interactive=False, verbosity=1)
 
 
-cmdclasses['test_demo'] = DemoTester
+cmdclasses["test_demo"] = DemoTester
 
 
 class Tester(Command):
@@ -88,19 +102,22 @@ class Tester(Command):
 
     def run(self):
         sys.dont_write_bytecode = True
-        os.environ['DJANGO_SETTINGS_MODULE'] = 'test_suite.settings_for_tests'
+        os.environ["DJANGO_SETTINGS_MODULE"] = "test_suite.settings_for_tests"
         import django
-        if hasattr(django, 'setup'):
+
+        if hasattr(django, "setup"):
             django.setup()
 
-        try:
-            from django.utils.unittest import TextTestRunner, defaultTestLoader
-        except ImportError:
-            from unittest import TextTestRunner, defaultTestLoader
+        from unittest import TextTestRunner, defaultTestLoader
 
         from test_suite import (
-            test_codecs, test_forms, test_models, test_validators,
-            test_widgets)
+            test_codecs,
+            test_forms,
+            test_models,
+            test_validators,
+            test_widgets,
+        )
+
         suite = defaultTestLoader.loadTestsFromModule(test_codecs)
         suite.addTests(defaultTestLoader.loadTestsFromModule(test_forms))
         suite.addTests(defaultTestLoader.loadTestsFromModule(test_models))
@@ -111,35 +128,44 @@ class Tester(Command):
         if result.wasSuccessful() is not True:
             raise SystemExit(int(bool(result.errors or result.failures)))
 
-cmdclasses['test'] = Tester
+
+cmdclasses["test"] = Tester
 
 setup(
-    name='django-select-multiple-field',
-    description='Select multiple choices in a single Django model field',
+    name="django-select-multiple-field",
+    description="Select multiple choices in a single Django model field",
     long_description=long_description,
     version=__version__,
-    license='BSD',
+    license="BSD",
     keywords=[
-        'select', 'select multiple', 'Django', 'model-field',
-        'Django-Select-Multiple-Field'],
-    author='Kelvin Wong',
-    author_email='code@kelvinwong.ca',
-    url='https://github.com/kelvinwong-ca/django-select-multiple-field',
-    classifiers=['Development Status :: 3 - Alpha',
-                 # 'Development Status :: 4 - Beta',
-                 'Environment :: Web Environment',
-                 'Framework :: Django',
-                 'Intended Audience :: Developers',
-                 'License :: OSI Approved :: BSD License',
-                 'Operating System :: OS Independent',
-                 'Programming Language :: Python',
-                 'Programming Language :: Python :: 2.7',
-                 'Programming Language :: Python :: 3',
-                 'Programming Language :: Python :: 3.2',
-                 'Programming Language :: Python :: 3.3',
-                 'Programming Language :: Python :: 3.4',
-                 'Programming Language :: Python :: 3.5',
-                 'Topic :: Internet :: WWW/HTTP'],
-    packages=['select_multiple_field'],
-    cmdclass=cmdclasses
+        "select",
+        "select multiple",
+        "Django",
+        "model-field",
+        "Django-Select-Multiple-Field",
+    ],
+    author="Kelvin Wong",
+    author_email="code@kelvinwong.ca",
+    url="https://github.com/kelvinwong-ca/django-select-multiple-field",
+    python_requires=">=3.10",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 4.2",
+        "Framework :: Django :: 5.2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+    packages=["select_multiple_field"],
+    cmdclass=cmdclasses,
 )
